a770dfe1e8dc0350a43ce89cace3dc66c87b5d74
Sebastian <sebastianparzych66@gmail.com>
2025-07-09 12:25:12 +0200

    test

--- Dockerfile
+++ Dockerfile
@@ -2,13 +2,9 @@ FROM python:3.11-slim
 
 WORKDIR /app
 
-COPY requirements.txt requirements.txt
+COPY requirements.txt .
 RUN pip install --no-cache-dir -r requirements.txt
 
-COPY app.py app.py
+COPY . .
 
-ENV PYTHONUNBUFFERED=1
-
-EXPOSE 5000
-
-CMD ["gunicorn", "app:app", "--bind", "0.0.0.0:5000"]
+CMD exec gunicorn app:app --bind 0.0.0.0:${PORT:-5000}
--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, jsonify
 import sqlite3
 import time
+import os
 
 app = Flask(__name__)
 DB_NAME = 'telemetry.db'
@@ -60,5 +61,6 @@ def get_latest_for_drone(drone_id):
             return jsonify({"error": "Brak danych dla tego drona"}), 404
 
 if __name__ == "__main__":
-    init_db()
-    app.run(host='0.0.0.0', port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+
